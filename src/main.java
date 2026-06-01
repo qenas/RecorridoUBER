536d9c353f1ffac64330d11ab32ea16065ce7bcc
Claudio Vasquez <clauvzq150@gmail.com>
2026-06-01 11:44:35 -0300

    Carga de grafo

--- src/main.java
+++ src/main.java
@@ -1,22 +1,35 @@
-import mapas.Lector;
+import grafos.grafoDirigido.GrafoDirigido;
+import lector.Lector;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class main {
     public static void main(String[] args) {
-        Lector mapas = new Lector();
+        Lector lector = new Lector();
 
-        mapas.setup();
+        lector.setup();
 
-        Map<String, Integer> aux = mapas.getMapaEsquinas();
+        Map<String, Integer> aux = lector.getPuntosUnicos();
 
         System.out.println(aux.size());
+
+        lector.mostrarCalles();
 
 
         /*for(String key : aux.keySet()) {
             System.out.println(aux.get(key));
         }*/
 
+        GrafoDirigido mapaCoste = new GrafoDirigido(aux.size());
+        mapaCoste.cargarGrafoVacio();
+
+
+        lector.cargarGrafoCoste(mapaCoste);
+
+        //mapaCoste.muestraGrafo();
+
+        Map<Integer, String> calles = new HashMap<>();
 
     }
 }
